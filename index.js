02ee3669e35309090e5e70ac87872576e585bf72
maryam shabanzadeh <shabanzadeh@yahoo.com>
2023-12-05 10:14:12 +0100

    install nodemon, connect with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const app = express()
+const mongoose = require("mongoose");
+const calander = require('./router/clander');
+const app = express();
+mongoose.connect('mongodb://localhost/test');
 
+
+app.use('/clander',calander)
 app.listen(5000,()=>{
     console.log('listening on port 5000...')
 })
