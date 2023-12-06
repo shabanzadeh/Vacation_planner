e50bde4a96b456efe26cb9186b89f321f1420de6
maryam shabanzadeh <shabanzadeh@yahoo.com>
2023-12-06 11:42:55 +0100

    connect with datenbank

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const calander = require('./router/clander');
+const calander = require('./src/router/clander');
 const app = express();
-mongoose.connect('mongodb://localhost/test');
-
+//mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true,useUnifiedTopology: true});
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.use('/clander',calander)
 app.listen(5000,()=>{
