c9845da2f1ec1cdbe08fc3bf891d7982623e6ac0
maryam shabanzadeh <shabanzadeh@yahoo.com>
2023-12-06 12:49:04 +0100

    connect with mongodb, define .env

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const calander = require('./src/router/clander');
 const app = express();
-//mongoose.connect('mongodb://localhost/test');
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true,useUnifiedTopology: true});
-const db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+require('dotenv').config();
+
+
+mongoose
+  .connect("mongodb://localhost:27017/planDb")
+  .then(()=> console.log('connected to mongodb'))
+  .catch(()=> console.log('could not connect to mongodb'));
+
 
 app.use('/clander',calander)
-app.listen(5000,()=>{
-    console.log('listening on port 5000...')
+app.listen(process.env.BACKEND_PORT,()=>{
+    console.log(`listening on port ${process.env.BACKEND_PORT}...`)
 })
 
